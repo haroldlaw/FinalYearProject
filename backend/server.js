d28448eb45259a1c7c8edf1df23ca391ca4eae0c
Harold Law <G00438467@atu.ie>
2025-11-13 15:52:22 +0000

    connect mongodb to backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,9 @@ app.use(cors());
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+// connect to database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Server is running');
