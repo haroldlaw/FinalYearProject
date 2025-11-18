b6cd776454778976d32931022b03db3b6b705186
Harold Law <G00438467@atu.ie>
2025-11-18 14:13:47 +0000

    add image route to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
 app.use(express.json());
@@ -24,6 +25,7 @@ app.get('/', (req, res) => {
 
 // API routes
 app.use('/api/users', userRoutes);
+app.use('/api/images', imageRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
